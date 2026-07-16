eee10774cc32193c8035969e4bed87077de9d961
YASWANTH <145116234+yaswanth-tech@users.noreply.github.com>
2026-07-16 12:41:04 +0530

    python day 1
    Day 1 Python practice programs and notes covering:
    - Python introduction
    - Variables
    - Dynamic typing
    - Comments
    - Boolean values
    - Variable swapping

---
+++ python-basics.py
@@ -0,0 +1,94 @@
+'''- in the year 1991 by Guido van Rossum
+
+What is python?
+
+High-level Language
+OOPs
+Interpreted Language 
+- The code will be executed line by line that's the reason python is called interpreted language
+'''
+
+num = 6
+print(num)
+num_2 = 9
+print(num_2)
+
+'''
+Dynamically typed language
+- No need to mention the type of data passing to the variable
+'''
+
+num='yash'
+num=9
+num=[5,6]
+print(type(num))
+
+'''
+Why python?
+
+easy to learn
+less syntax
+easy to understand
+cross platform
+open-source
+large num of library
+
+Comments
+
+single-line (#)
+-this is used to explain line in the code
+multi-line 
+-used to comment multiple lines using ''' '''. """ """
+
+Variables
+
+this is used to store datatypes
+
+good way to define a variable 
+-small 
+-cap
+'''
+_num = 90
+num = 9
+Num = 99
+num_2 = 7
+print(_num, num, Num, num_2) 
+
+'''
+Bad way
+-special character(&,...)
+-first position number(7num)
+-spaces( n n)
+
+
+Boolean type
+'''
+
+num = 7
+num_2 = 77
+print(num == num_2) 
+print(num != num_2) 
+
+'''multiple variables with single value'''
+num = num_2 = num_3 = 77
+print(num) == 77
+print(num_2) == 77
+print(num_3) == 77
+
+'''multiple variables with multiple values'''
+num, num_2 = 7, 77
+
+'''swapping variable without using three variable'''
+num = 7
+num_2 = 77
+num, num_2 == num_2, num
+print(num) == 77
+
+
+
+
+
+
+
+
+
